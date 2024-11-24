fd3b514d92b7c7c511ee6e0dbd6e9c0c824a14ce
Alex Manev <mane0003@hz.nl>
2024-11-24 04:21:52 +0100

    adding tasks implemented

---
+++ src/Components/TodoTask.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { ITask } from '../interfaces'
+
+interface Props {
+    task:ITask;
+}
+
+const TodoTask = ({task} : Props) => {
+  return (
+    <div>
+      {task.taskName}
+      {task.deadline}
+    </div>
+  )
+}
+
+export default TodoTask
